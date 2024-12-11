d5d1dedc0470f652c4aef77dcd08733704a65a3d
Miroslav Horvatov <52826419+kimleta@users.noreply.github.com>
2024-12-11 14:47:25 +0100

    Compatibility with multiple articles

--- src/Classes/GptClass.php
+++ src/Classes/GptClass.php
@@ -10,28 +10,29 @@ use Contao\Database;
 use Contao\ContentModel;
 use Codebuster\GptBundle\Models\ContentElementsModel;
 
-class GptClass {
+class GptClass
+{
     private static function prepareContent($objArticles): string
     {
         $strContent = '';
         $customFields = [];
-        if(Config::get("gpt_custom_fields")) {
+        if (Config::get("gpt_custom_fields")) {
             $customFields = unserialize(Config::get("gpt_custom_fields"));
         }
-
         // get Content from all Articles
         if ($objArticles !== null) {
             foreach ($objArticles as $article) {
-                
+
                 $headline = unserialize(strip_tags(nl2br($article->headline)));
-                if($headline["value"]) {
-                    $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $headline["value"]))).' - ';
+                if ($headline["value"]) {
+                    $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $headline["value"]))) . ' - ';
                 }
                 $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $article->text)));
-                if(!empty($customFields)) {
-                    foreach($customFields AS $customField) {
+
+                if (!empty($customFields)) {
+                    foreach ($customFields as $customField) {
                         // dont regard serialized content
-                        if(!is_array(unserialize($article->$customField))) {
+                        if (!is_array(unserialize($article->$customField))) {
                             $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $article->$customField)));
                         }
                     }
@@ -39,7 +40,6 @@ class GptClass {
 
             }
         }
-
         // Todo: do max chars even smarter
         return $strContent;
     }
@@ -52,18 +52,24 @@ class GptClass {
      * @return Object
      * @throws Exception If content isn't found
      */
-   public static function getContent($table, $id): string {
+    public static function getContent($table, $id): string
+    {
 
 
         //gets correct article of page
-        if($table == 'tl_page') {
-            $id = ArticleModel::findByPid($id)->id;
+        if ($table == 'tl_page') {
+            $articles = ArticleModel::findByPid($id);
+            $ids = [];
+            foreach ($articles as $v) {
+                $ids[] = $v->id;
+            }
+
             $table = "tl_article";
         }
 
 
-        return self::prepareContent(self::getArticle($table, $id));
-   }
+        return self::prepareContent(self::getArticle($table, $ids));
+    }
 
     /**
      * Checks if table is allowed to be accessed
@@ -72,50 +78,56 @@ class GptClass {
      * @param String $table
      * @return Boolean
      */
-   protected static function isValidTable($table) {
+    protected static function isValidTable($table)
+    {
         $tables = Config::get('gpt_allowed_tables');
         $tables = StringUtil::deserialize($tables);
 
         foreach ($tables as $k => $v) {
-            if($v == $table) {
+            if ($v == $table) {
                 return true;
             }
         }
-   }
-   
-   /**
+    }
+
+    /**
      * Fetches Article of given table
      * 
      * @param String $table
-     * @param int $id
+     * @param array $id
      * @return Object
      * @throws Exception If content isn't found
      */
-   public static function getArticle($table, $id) {
+    public static function getArticle(string $table, array $ids)
+    {
 
         //is table valid?
-        if(\Contao\Database::getInstance()->tableExists($table) && self::isValidTable($table)) {
-      
+        if (\Contao\Database::getInstance()->tableExists($table) && self::isValidTable($table)) {
+
 
             $blnHidden = false;
-            if(Config::get("gpt_hidden_elements") === true) {
+            if (Config::get("gpt_hidden_elements") === true) {
                 $blnHidden = true;
 
             }
+            $objArticles = [];
+            foreach ($ids as $id) {
+                //is record valid?
+                $record = $GLOBALS['TL_MODELS'][$table]::findBy(["id=?", "published=?"], [$id, $blnHidden ? 0 : 1]);
 
-            //is record valid?
-            $record = $GLOBALS['TL_MODELS'][$table]::findBy(["id=?","published=?"], [$id, $blnHidden ? 0 : 1]);
-            
-            if($record) {
-                // get contentelements from article
-                $objArticles = ContentModel::findBy(["pid=?",'ptable=?'], [$id,$table]);
-                return $objArticles;
-                
-            } else {
-                throw new Exception("Record with ID $id not found.");
+                if ($record) {
+                    // get contentelements from article
+                    $objArticles[] = ContentModel::findBy(["pid=?", 'ptable=?'], [$id, $table]);
+                } else {
+                    throw new Exception("Record with ID $id not found.");
+                }
             }
+
+
+            return $objArticles;
+
         } else {
             throw new Exception("Table not found. Check $table exists and has been approved in the settings.");
         }
-   }
+    }
 }
