40954eee8d6799d0ec03353578e31901788f10bf
Miroslav Horvatov <52826419+kimleta@users.noreply.github.com>
2025-03-11 16:24:02 +0100

    Update GptClass.php

--- src/Classes/GptClass.php
+++ src/Classes/GptClass.php
@@ -9,6 +9,9 @@ use Contao\StringUtil;
 use Contao\Database;
 use Contao\ContentModel;
 use Codebuster\GptBundle\Models\ContentElementsModel;
+use Contao\Controller;
+
+
 
 class GptClass
 {
@@ -23,29 +26,44 @@ class GptClass
         // get Content from all Articles
         if ($objArticles !== null) {
             foreach ($objArticles as $article) {
+                if(!$article->invisible){
+                    foreach ($article as $contentElement) {
 
-                foreach ($article as $contentElement) {
+                        // Load all palettes
+                        Controller::loadDataContainer('tl_content');
 
-                    if ($contentElement->type != "module") {
+                        if ($contentElement->type != "module") {
 
-                        $headline = unserialize(strip_tags(nl2br($contentElement->headline)));
-                        if ($headline["value"]) {
-                            $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $headline["value"]))) . ' - ';
-                        }
-                        $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $contentElement->text)));
+                            $pallete = $GLOBALS['TL_DCA']['tl_content']['palettes'][$contentElement->type];
 
-                        if (!empty($customFields)) {
-                            foreach ($customFields as $customField) {
-                                // dont regard serialized content
-                                if (!is_array(unserialize($contentElement->$customField))) {
-                                    $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $contentElement->$customField)));
+                            if (self::findFieldInPallete($pallete, 'headline')) {
+                                $headline = unserialize(strip_tags(nl2br($contentElement->headline)));
+                                if ($headline["value"]) {
+                                    $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $headline["value"]))) . ' - ';
                                 }
                             }
+
+                            if (self::findFieldInPallete($pallete, 'text')) {
+                                $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $contentElement->text)));
+                            }
+
+                            if (!empty($customFields)) {
+                                foreach ($customFields as $customField) {
+                                    // dont regard serialized content
+                                    if (self::findFieldInPallete($pallete, $customField)) {
+                                        if (!is_array(unserialize($contentElement->$customField))) {
+                                            $strContent .= strip_tags(trim(preg_replace('/\s+/', ' ', $contentElement->$customField)));
+                                        }
+                                    }
+                                }
+                            }
+                            
                         }
                     }
                 }
             }
         }
+
 
         // Todo: do max chars even smarter
         return $strContent;
@@ -61,17 +79,20 @@ class GptClass
      */
     public static function getContent($table, $id): string
     {
+
         //gets correct article of page
         if ($table == 'tl_page') {
             $articles = ArticleModel::findByPid($id);
-            
+            $ids = [];
             foreach ($articles as $v) {
                 $ids[] = $v->id;
             }
 
             $table = "tl_article";
-        } else {
-            $ids = [$id];
+        }
+
+        if (empty($ids)) {
+            $ids[] = $id;
         }
 
         return self::prepareContent(self::getArticle($table, $ids));
@@ -106,29 +127,45 @@ class GptClass
      */
     public static function getArticle(string $table, array $ids)
     {
+
+
         //is table valid?
         if (\Contao\Database::getInstance()->tableExists($table) && self::isValidTable($table)) {
+
 
             $blnHidden = false;
             if (Config::get("gpt_hidden_elements") === true) {
                 $blnHidden = true;
+
             }
+
             $objArticles = [];
             foreach ($ids as $id) {
-                //is record valid?
+
                 $record = $GLOBALS['TL_MODELS'][$table]::findBy(["id=?", "published=?"], [$id, $blnHidden ? 0 : 1]);
-                
+
                 if ($record) {
                     // get contentelements from article
                     $objArticles[] = ContentModel::findBy(["pid=?", 'ptable=?'], [$id, $table]);
-                } else {
-                    throw new Exception("Record with ID $id not found.");
-                }
+                } 
             }
 
+
             return $objArticles;
+
         } else {
             throw new Exception("Table not found. Check $table exists and has been approved in the settings.");
         }
+    }
+
+    /**
+     * Find specific field in palette, so we don't send data that is not displayed in frontend.
+     * @param string $pallete
+     * @param string $field
+     * @return bool
+     */
+    private static function findFieldInPallete(string $pallete, string $field): bool
+    {
+        return (bool) preg_match('/\b' . preg_quote($field, '/') . '\b/', $pallete);
     }
 }
